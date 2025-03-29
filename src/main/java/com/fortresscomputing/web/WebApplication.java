06bba16836905415cf0e0d3b6d7787cc3533f2d9
Sebastian Cheung <sebastian_cheung@yahoo.com>
2025-03-29 23:45:33 +0000

    Tidied up

--- src/main/java/com/fortresscomputing/web/WebApplication.java
+++ src/main/java/com/fortresscomputing/web/WebApplication.java
@@ -7,16 +7,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class WebApplication implements CommandLineRunner {
 
 	@Autowired
 	OpenAiChatModel chatModel;
 
+	// http://localhost:8080/joke/dad
+
+	@GetMapping("/joke/{type}")
+	public String funnyJokes(@PathVariable String type) {
+		String input = String.format("Tell me a random %s joke", type);
+		Prompt prompt = new Prompt(input);
+		ChatResponse response = chatModel.call(prompt);
+		return response.getResult().getOutput().getText();
+	}
+
+	// Console display from CommandLineRunner
 	@Override
 	public void run(String... args) throws Exception {
-		Prompt prompt = new Prompt("What is the capital of Hong Kong?");
+		Prompt prompt = new Prompt("What is the capital of Thailand?");
 		ChatResponse response = chatModel.call(prompt);
 		System.out.println(response.getResult().getOutput().getText());
 	}
